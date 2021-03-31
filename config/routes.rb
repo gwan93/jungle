ac7dff1ee152c3aa2aa08df66962bb719368acf1
Gio Wan <gio_wan@live.ca>
2021-03-31 15:19:25 -0600

    add user and sessions to let users login and register

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,15 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  # Session routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # Login/register routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
